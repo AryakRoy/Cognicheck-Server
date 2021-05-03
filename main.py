e654363df2de1ccbc9d31c9d6afe4f7c4de25362
AryakRoy <aryakroy63@gmail.com>
2021-05-03 16:14:42 +0530

    Deploy Ready RESTFUL API

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, request,jsonify
 from predictor import Predictor_Model
 from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
-
+CORS(app)
 
 class Analyzer(Resource):
   def __init__(self):
@@ -13,13 +14,16 @@ class Analyzer(Resource):
     self.req_parser = parser
     self.pred_model = Predictor_Model()
 
+  def get(self):
+    return jsonify({'result' : 'Hello'})
+    
   def post(self):
     args = self.req_parser.parse_args()
     mriData = args["mri"]
     result = self.pred_model.analyze(mriData)
     return jsonify({'result' : f'{result}'})
 
-api.add_resource(Analyzer,"/predictor")  
+api.add_resource(Analyzer,"/")  
 
 if __name__ == "__main__":
   app.run(debug=True)
